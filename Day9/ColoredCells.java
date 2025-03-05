b08da4e6ebab9dbd38cfc92bb6a09c035444ec0e
Harishvar <harishvar.kanna@gmail.com>
2025-03-06 00:15:57 +0530

    Add Day 9

---
+++ Day9/ColoredCells.java
@@ -0,0 +1,14 @@
+class ColoredCells {
+    public long coloredCellsRecursion(int n) {
+        if( n == 1){
+            return 1;
+        }
+        return 4*(n - 1) + coloredCells(n - 1);
+    }
+
+
+public long coloredCellsFormula(int n) {
+        return 2 * ((long)n * n - n) + 1;
+    }
+
+}
